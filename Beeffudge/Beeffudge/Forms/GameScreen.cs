dc289b576e64f44e2555737d7ac5fe775c11c90b
Njamb <njamb94@hotmail.com>
2017-10-07 20:36:40 +0200

    Dodat subscriber za chat, pitanja. Slanje unetog odgovora, slanje odabira dugmeta, slanje poruke ubaceno.

--- Beeffudge/Beeffudge/Forms/GameScreen.cs
+++ Beeffudge/Beeffudge/Forms/GameScreen.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using ZeroMQ;
@@ -15,16 +16,34 @@ namespace Beeffudge.Forms {
         public List<string> answers;
         public List<Button> answerButtons;
 
-        public GameScreen () {
+        private Dictionary<int, MessagePassing> dictSockets;
+        private Lobby parent;
+        private string _username;
+        private string[] _payers;
+
+        public GameScreen(Lobby parent) {
             InitializeComponent();
 
+            // Get lobby reference:
+            this.parent = parent;
+            _username = parent._Name;
+            _payers = parent.Players;
+            // Get reference for message workers:
+            dictSockets = parent.DictSockets;
+
+            // Start thread to listen for chat messages:
+            Thread chatSubscribeThread = new Thread(() => UpdateChat());
+            chatSubscribeThread.Start();
+
+            // Prvi put uzmemo pitanje i odgovore od servera:
+            GetQuestionFromServer();
         }
 
-        private void txtChat_TextChanged (object sender, EventArgs e) {
+        private void txtChat_TextChanged(object sender, EventArgs e) {
 
         }
 
-        private void GameScreen_Load (object sender, EventArgs e) {
+        private void GameScreen_Load(object sender, EventArgs e) {
             answerButtons = new List<Button>();
             answerButtons.Add(btnAnswer1);
             answerButtons.Add(btnAnswer2);
@@ -33,25 +52,26 @@ namespace Beeffudge.Forms {
             answerButtons.Add(btnAnswer5);
         }
 
-        private void btnChatSend_Click (object sender, EventArgs e) {
-            string chatMessage = txtChat.Text;
+        private void btnChatSend_Click(object sender, EventArgs e) {
             //Send chatMessage to server for our player
+            SendChatMessage();
+
             //TEST
-            List<string> someAnswers = new List<string>() { "first", "second", "third", "fourth", "fifth" };
-            InstantiateAnswerButtons(someAnswers);
+            //List<string> someAnswers = new List<string>() { "first", "second", "third", "fourth", "fifth" };
+            //InstantiateAnswerButtons(someAnswers);
         }
 
-        public void UpdateQuestionText (string newQuestion) {
+        public void UpdateQuestionText(string newQuestion) {
             lblQuestion.Text = newQuestion;
         }
 
-        public void HideAnswerButtons () {
+        public void HideAnswerButtons() {
             for (int i = 0; i < answerButtons.Count; i++) {
                 answerButtons[i].Visible = false;
             }
         }
 
-        public void InstantiateAnswerButtons (List<string> allAnswers) {
+        public void InstantiateAnswerButtons(List<string> allAnswers) {
             answers = Shuffle(allAnswers);
             for (int i = 0; i < answers.Count; i++) {
                 answerButtons[i].Text = answers[i];
@@ -59,7 +79,7 @@ namespace Beeffudge.Forms {
             }
         }
 
-        public static List<string> Shuffle (List<string> list) {
+        public static List<string> Shuffle(List<string> list) {
             int n = list.Count;
             Random rng = new Random();
             while (n > 1) {
@@ -72,37 +92,118 @@ namespace Beeffudge.Forms {
             return list;
         }
 
-        private void btnAnswer1_Click (object sender, EventArgs e) {
-            sendSelectedAnswerToServer(0);
+        private void btnAnswer1_Click(object sender, EventArgs e) {
+            SendChoiceToServer(0);
         }
 
-        private void btnAnswer2_Click (object sender, EventArgs e) {
-            sendSelectedAnswerToServer(1);
+        private void btnAnswer2_Click(object sender, EventArgs e) {
+            SendChoiceToServer(1);
         }
 
-        private void btnAnswer3_Click (object sender, EventArgs e) {
-            sendSelectedAnswerToServer(2);
+        private void btnAnswer3_Click(object sender, EventArgs e) {
+            SendChoiceToServer(2);
         }
 
-        private void btnAnswer4_Click (object sender, EventArgs e) {
-            sendSelectedAnswerToServer(3);
+        private void btnAnswer4_Click(object sender, EventArgs e) {
+            SendChoiceToServer(3);
         }
 
-        private void btnAnswer5_Click (object sender, EventArgs e) {
-            sendSelectedAnswerToServer(4);
+        private void btnAnswer5_Click(object sender, EventArgs e) {
+            SendChoiceToServer(4);
         }
 
-        private void sendSelectedAnswerToServer (int answerNumber) {
+        private void SendChoiceToServer(int answerNumber) {
             string answer = answers[answerNumber];
             //send selected answer to server for our player
+
+            string dummy = dictSockets[Lobby.REP_CHOICE].GetMessage();
+            dictSockets[Lobby.REP_CHOICE].SendMessage(answer);
+
+            // Iskljucimo dugmice do sledeceg pitanja:
+            foreach (Button btn in answerButtons) {
+                btn.Enabled = false;
+            }
         }
 
-        private void btnEnterAnswer_Click_1 (object sender, EventArgs e) {
+        private void btnEnterAnswer_Click_1(object sender, EventArgs e) {
             string answer = txtAnswer.Text;
             //Send answer to server for our player
+            string dummy = dictSockets[Lobby.REP_ANSWER].GetMessage();
+            dictSockets[Lobby.REP_ANSWER].SendMessage(answer);
+
+            // Iskljucimo dugme da ne salje 
+            // bespotrebno kada ne treba:
+            btnEnterAnswer.Enabled = false;
+
+
+
             //TEST
-            HideAnswerButtons();
-            UpdateQuestionText("A new question fosho");
+            //HideAnswerButtons();
+            //UpdateQuestionText("A new question fosho");
+        }
+
+        private void SendChatMessage() {
+            string dummy = dictSockets[Lobby.REP_CHAT_MSG].GetMessage();
+            // Send as 'Name: msg'
+            dictSockets[Lobby.REP_CHAT_MSG].SendMessage(string
+                .Format("{0}: {1}", Name, txtChatSend.Text.ToString()));
+        }
+
+        // Own thread!
+        private void UpdateChat() {
+            string msg = string.Empty;
+            while (true)
+            {
+                msg = dictSockets[Lobby.SUB_CHAT].GetMessage();
+                txtChat.Text = txtChat.Text + msg + Environment.NewLine;
+            }
+        }
+
+        // TODO: Pozvati svaki put kad se runda zavrsi \i sledeci red ispod
+        // i kada istekne tajmer i kada se ocekuje prikaz odgovora!
+        private void GetQuestionFromServer() {
+            string question = string.Empty;
+
+            question = dictSockets[Lobby.SUB_QUESTION].GetMessage();
+
+            QuestionWithAnswers qwa = new QuestionWithAnswers(question);
+
+            // Question now has only the question:
+            question = qwa.QuestionString;
+            
+            // Received answers from question:
+            answers = qwa.AnswersString;
+
+            // Update question text:
+            UpdateQuestionText(question);
+
+            // Ako je poslato pitanje sa unesenim odgovorima:
+            // '#' je escape simbol za prvo slanje pitanja.
+            if (!answers[0].Equals("#"))
+                // Instantiate answers:
+                InstantiateAnswerButtons(answers);
+            else {
+                // U suprotnom treba da se samo pitanje postavi
+                // i ugase dugmici.
+                HideAnswerButtons();
+            }
+        }
+
+        // Question received looks like this:
+        // 'questionQuqestion bla bla ___ ?$answer1$answer2$..etc'
+        partial class QuestionWithAnswers
+        {
+            public string QuestionString { get; private set; }
+            public List<string> AnswersString { get; private set; }
+
+            public QuestionWithAnswers(string questionWithAnswers) {
+                string[] questionSeparated = questionWithAnswers.Split('$');
+                QuestionString = questionSeparated[0];
+
+                for (int i = 1; i < questionSeparated.Length; i++) {
+                    AnswersString.Add(questionSeparated[i]);
+                }
+            }
         }
     }
 }
